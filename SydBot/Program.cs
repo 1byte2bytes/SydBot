30a5dc81f87cb98d8ab1e563a8bc88be08a4c847
Sydney Erickson <sydneyerickson@icloud.com>
2017-10-08 11:34:11 -0700

    VS Template

---
+++ SydBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SydBot
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
