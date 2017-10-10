a5022d8f305c353f73adae9e7351c67662380980
Sydney Erickson <sydneyerickson@icloud.com>
2017-10-10 14:17:06 -0700

    Create Discord base DLL

---
+++ Discord/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Discord
+{
+    public class Class1
+    {
+        //<summary>Obtain the Discord gateway URL, connect to it, and login. Also sets up the heartbeat tick.</summary>
+        //<param name="token">Your Discord bot token</param>
+        public void Connect(string token)
+        {
+            
+        }
+    }
+}
